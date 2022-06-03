f05bb8b108986914a143004828c50008c71e4355
Guanghui Wang <guanghui-wang@foxmail.com>
2022-06-03 16:35:15 +0800

    feat: save data to db

--- util/http.js
+++ util/http.js
@@ -3,8 +3,13 @@ import axios from 'axios';
 const BACKEND_URL =
   'https://react-native-course-3cceb-default-rtdb.firebaseio.com';
 
-export function storeExpense(expenseData) {
-  axios.post(BACKEND_URL + '/expenses.json', expenseData);
+export async function storeExpense(expenseData) {
+  const response = await axios.post(
+    BACKEND_URL + '/expenses.json',
+    expenseData,
+  );
+  const id = response.data.name;
+  return id;
 }
 
 export async function fetchExpenses() {
