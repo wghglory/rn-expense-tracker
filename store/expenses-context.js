24fd6d4bc40e0a436ac5b20216571a873318ba9e
Guanghui Wang <guanghui-wang@foxmail.com>
2022-06-03 16:25:40 +0800

    feat: fetch data from firebase

--- store/expenses-context.js
+++ store/expenses-context.js
@@ -60,6 +60,7 @@ const DUMMY_EXPENSES = [
 export const ExpensesContext = createContext({
   expenses: [],
   addExpense: ({ description, amount, date }) => {},
+  setExpenses: expenses => {},
   deleteExpense: id => {},
   updateExpense: (id, { description, amount, date }) => {},
 });
@@ -69,6 +70,8 @@ function expensesReducer(state, action) {
     case 'ADD':
       const id = new Date().toString() + Math.random().toString();
       return [{ ...action.payload, id: id }, ...state];
+    case 'SET':
+      return action.payload;
     case 'UPDATE':
       const updatableExpenseIndex = state.findIndex(
         expense => expense.id === action.payload.id,
@@ -87,9 +90,14 @@ function expensesReducer(state, action) {
 
 function ExpensesContextProvider({ children }) {
   const [expensesState, dispatch] = useReducer(expensesReducer, DUMMY_EXPENSES);
+  // const [expensesState, dispatch] = useReducer(expensesReducer, []);
 
   function addExpense(expenseData) {
     dispatch({ type: 'ADD', payload: expenseData });
+  }
+
+  function setExpenses(expenses) {
+    dispatch({ type: 'SET', payload: expenses });
   }
 
   function deleteExpense(id) {
@@ -102,9 +110,10 @@ function ExpensesContextProvider({ children }) {
 
   const value = {
     expenses: expensesState,
-    addExpense,
-    deleteExpense,
-    updateExpense,
+    setExpenses: setExpenses,
+    addExpense: addExpense,
+    deleteExpense: deleteExpense,
+    updateExpense: updateExpense,
   };
 
   return (
